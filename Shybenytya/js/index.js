4da5b98ac9fe7f70ff080def54614f45a958642d
rimbold1 <38472212+rimbold1@users.noreply.github.com>
2022-02-16 23:07:03 +0200

    Add files via upload
    beginning of a project

---
+++ Shybenytya/js/index.js
@@ -0,0 +1,30 @@
+// let alphabet = "abcdefghijklmnopqrstuvwxyz";
+// let newString = [];
+
+// while (newString.length < 6) {
+//     let randomLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
+//     newString.push(randomLetter);
+// }
+
+// console.log(newString.join(""));
+
+
+// let input = "Javascript is awesome!";
+// let output = "";
+
+// for (let i = 0; i < input.length; i++) {
+//     const element = input[i];
+//     if (element === "a") {
+//         output += "4"
+//     } else if (element === "e") {
+//         output += "3"
+//     } else if (element === "i") {
+//         output += "1"
+//     } else if (element === "o") {
+//         output += "0"
+//     }else {
+//         output += element;
+//     }
+// }
+
+// console.log(output);
