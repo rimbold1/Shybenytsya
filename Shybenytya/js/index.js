d819bba5b3e51422d3fed966700cf4ae7c4cd8d7
rimbold1 <ivivzu2@gmail.com>
2023-07-11 16:10:05 +0300

    commit 11.07.23

--- Shybenytya/js/index.js
+++ Shybenytya/js/index.js
@@ -1,33 +1,45 @@
 
-let wordsArr = ["bread", "gamepad", "fish", "dog", "powerbank", "smartphone", "book", "elephant", "boat", "window", "javascript"];
+let wordsArr = [["bread", "Food. Made from grain"], 
+["gamepad", "Controll gadget for playing games."], 
+["fish", "Animal. Loves water."], 
+["dog", "Animal. Humans best friend"], 
+["powerbank", "Thing that allow you to charge your smartphone."], 
+["smartphone", "A thing that we use very often and carry it with us."], 
+["book", "Paper media."], ["elephant", "Animal with large ears."], 
+["boat", "Floating transport"], 
+["window", "glassed barrier between house and outdoors"], 
+["javascript", "a programming language"]];
 let word = wordsArr[Math.floor(Math.random() * wordsArr.length)]; // random word chosen from array of words.
-// console.log(word)
 
 let answerArray = []; // Цей масив заповнюється символом нижнього підкреслення рівно на стільки, скільки в загаданого слова букв.
-for (let i = 0; i < word.length; i++) {
+for (let i = 0; i < word[0].length; i++) {
     answerArray[i] = "_";
 }
 
-let remainingLetters = word.length; // кількість букв в загаданому слові.
+let remainingLetters = word[0].length; // кількість букв в загаданому слові.
+let attempts = 5;
 
 while (remainingLetters > 0) {
-    alert(answerArray.join(" "));
+    alert(`${answerArray.join(" ")} ${word[1]} кількість спроб: ${attempts}`);
 
     let guess = prompt("Загадайде літеру, або натисніть cancel щоб зупинит гру.");
 
-    if (guess === null) {
+    if (guess === null || guess === "") {
+        alert("Гру припинено.");
         break;
     } else if (guess.length !== 1) {
-        alert("Будь-ласка введіть не більше 1 літери.")
+        alert("Будь-ласка введіть не більше 1 літери.");
     } else {
-        for (let j = 0; j < word.length; j++) {
-            if (word[j] === guess) {
-                answerArray[j] = guess;
+        for (let j = 0; j < word[0].length; j++) {
+            if (word[0][j] === guess.toLowerCase()) {
+                answerArray[j] = guess.toLowerCase();
                 remainingLetters--;
-            }  
+            }
         }
     }
 }
 
-alert(answerArray.join(" "));
-alert("Хороша робота! Відповідь була " + word);
+if (answerArray.join("") === word[0]) {
+    alert(answerArray.join(" "));
+    alert("Хороша робота! Відповідь була " + word[0]);
+}
